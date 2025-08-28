f28732b25d26852531a0c1b6f14feafaa1708ec2
Olakunlevpn <olakunlevpn@live.com>
2025-08-28 22:01:11 +0100

    Day 2025-08-28: Update stats

--- execute.sh
+++ execute.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-#python simulation.py
-#python utils/log_manager.py
+python simulation.py
+python utils/log_manager.py
 
 git add .
 git commit -m "Day $(date '+%Y-%m-%d'): Update stats"
